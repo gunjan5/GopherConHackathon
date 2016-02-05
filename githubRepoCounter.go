4e4108bdaf44417bab8cdf642d361c858b0c1219
Gunjan Patel <patelgunjan5@gmail.com>
2016-02-04 23:17:39 -0800

    github API

---
+++ githubRepoCounter.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var id int
+
+	fmt.Scanf("%d", &id)
+	if id < 1 {
+		fmt.Printf("Input must be > 1, you entered: %d \n", id)
+		fmt.Errorf("Input must be > 1, you entered: %d \n", id)
+		os.Exit(1)
+	}
+
+	resp, _ := http.Get("https://api.github.com/user/" + strconv.Itoa(id)) //5966968
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	var obj map[string]interface{}
+
+	err := json.Unmarshal(body, &obj)
+	if err != nil {
+		panic(err)
+	}
+	repos := obj["public_repos"]
+	i, ok := repos.(float64) //need to assert the interface to float64 first (can't assert it to int directly!!)
+	if !ok {
+		fmt.Println("User ID not found: error converting response to float64")
+		fmt.Errorf("User ID not found: error converting response to float64")
+		os.Exit(1)
+	}
+	fmt.Println(int(i))
+
+}
